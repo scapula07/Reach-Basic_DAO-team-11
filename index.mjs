63a1affaed5b450050e6438ca8f56ac5378c7a06
scapula07 <bartholomewonogwu@yahoo.com>
2022-09-18 13:07:32 -0700

    changes to index.mjs file n update to readme

--- index.mjs
+++ index.mjs
@@ -4,7 +4,7 @@ const stdlib = loadStdlib(process.env);
 
 const startingBalance = stdlib.parseCurrency(100);
 
-const accGovernor=await stdlib.newTestAccount(2, startingBalance);
+const accGovernor= await stdlib.newTestAccount( startingBalance);
 console.log('Hello, Alice and Bob!');
 
 console.log('Launching...');
@@ -15,11 +15,40 @@ const ctcGovernor = accGovernor.contract(backend);
 console.log('Starting backends...');
 
 const voters=[]
-const  startingVoting=()=>{
-  
-  const acc =await stdlib.newTestAccount(startingBalance)
-  const ctc=acc.Contract(backend,ctcGovernor.getInfo())
-  voters
+
+let done = false; // <---------------------------- This was added to ensure the code will wait
+
+const  startingVoting=async()=>{
+  const newAcc=async(who)=>{
+  const acc = await stdlib.newTestAccount(startingBalance)
+  const ctc = acc.contract(backend,ctcGovernor.getInfo())
+  voters.push({who,acc})
+
+  const randomBoolean = Math.random() >= 0.5;
+
+  try{
+    const value = await ctc.apis.Voter.vote(randomBoolean)
+    const word = value ? "voted for" : "voted against"
+
+    console.log(`${who}  ${word} proposal`)
+  }catch(e){
+    console.log(e)
+  }
+  }
+
+  await newAcc("Bob")
+  await newAcc("Thomas")
+  await newAcc("Nick")
+  await newAcc("sam")
+  await newAcc("James")
+  await newAcc("Joy")
+  await newAcc("Jane")
+
+///---------- Here is the actual waiting code
+  while ( ! done ) {
+    await stdlib.wait(1);
+}
+
 }
 
 
@@ -32,16 +61,21 @@ await ctcGovernor.p.Governor({
           title:"Good",
           description:"A good thing",
           ticketPrice: stdlib.parseCurrency(5),
-          deadline: 10,
+          deadline: 25,
           
         }),
   proposalReady:()=>{
     startingVoting()
   },
   showOutcome:(title, forProposal, againstProposal)=>{
-    console.log(`${title} poll outcome: ${ forProposal} to ${againstProposal}`)
+    console.log(` alice saw proposal ${title} poll outcome: ${ forProposal} to ${againstProposal}`)
+  },
+  showTimeout:(timeout)=>{
+    console.log(`${timeout} dealine reach `)
   }
-
 })
 
 console.log('Goodbye, Alice and Bob!');
+
+/// And here is where it is cleared
+done = true; 
